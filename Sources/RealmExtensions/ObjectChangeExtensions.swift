e2abe6fd92c78057b33002ff3413d60e3e461b7f
Nate <nathan.mann@me.com>
2020-10-23 13:47:36 -0400

    made properties public

--- Sources/RealmExtensions/ObjectChangeExtensions.swift
+++ Sources/RealmExtensions/ObjectChangeExtensions.swift
@@ -4,7 +4,7 @@ import RealmSwift
 
 extension ObjectChange {
 
-    var objectChanged: T? {
+    public var objectChanged: T? {
         guard case .change(let object, _) = self else { return nil }
         return object
     }
--- Sources/RealmExtensions/RealmCollectionChangeExtensions.swift
+++ Sources/RealmExtensions/RealmCollectionChangeExtensions.swift
@@ -4,12 +4,12 @@ import RealmSwift
 
 extension RealmCollectionChange where CollectionType: RealmCollection {
 
-    var insertions: [CollectionType.Element] {
+    public var insertions: [CollectionType.Element] {
         guard case .update(let objects, _, let insertions, _) = self else { return [] }
         return insertions.map({ Array(objects)[$0] })
     }
 
-    var modifications: [CollectionType.Element] {
+    public var modifications: [CollectionType.Element] {
         guard case .update(let objects, _, _, let modifications) = self else { return [] }
         return modifications.map({ Array(objects)[$0] })
     }
