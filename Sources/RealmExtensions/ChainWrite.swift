806cabe72ff29bdcb06701e15f9094095a543ca5
Nate Mann <nate@MB-MWY3DVJ627.local>
2022-08-17 21:00:19 -0400

    formatting

--- Sources/RealmExtensions/ChainWrite.swift
+++ Sources/RealmExtensions/ChainWrite.swift
@@ -15,7 +15,6 @@ extension Realm {
     }
   }
   
-  
   ///Convience method for `realm.write(withoutNotifying:)`. This first checks if the realm is currently in a write transaction. If it is, it simply calls `f`. If it is not in a write transaction, it will call `f` within `write`.
   public func chainWrite(withoutNotifying tokens: [NotificationToken], _ f: () -> ()) {
     if self.isInWriteTransaction {
